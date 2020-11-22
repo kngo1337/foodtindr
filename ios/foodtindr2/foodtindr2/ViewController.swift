03a480079673a8a32c829cb178b3c789c94446b0
Alan Nguyen <alan.nguyen@uscontracting.us>
2020-11-22 12:26:59 -0500

    git ignore

---
+++ ios/foodtindr2/foodtindr2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foodtindr2
+//
+//  Created by Hao on 11/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
