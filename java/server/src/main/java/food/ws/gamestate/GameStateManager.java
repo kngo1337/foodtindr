e6204d985f1649f9c29a8906556c1e943260c88c
Thien-An Nguyen <tanathanng123@gmail.com>
2020-11-22 18:49:55 -0500

    GameStateManager

--- java/server/src/main/java/food/ws/gamestate/GameStateManager.java
+++ java/server/src/main/java/food/ws/gamestate/GameStateManager.java
@@ -4,78 +4,79 @@ import java.util.ArrayList;
 
 public class GameStateManager {
 
-    Room gameRoom = new Room();
-    boolean gameStarted = false;
-    ArrayList<String> names = new ArrayList<String>();
-    int count = 0;
+    ArrayList<Room> gameRoom = new ArrayList<Room>();
 
     public GameStateManager(){
-        //Room gameRoom = new Room();
-        //Room.players = new ArrayList<Player>();
+
     }
 
     private String createNewGame(String playerName){
+        Room newGame = new Room();
         String randomString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        StringBuffer newGameCode = new StringBuffer(4);//Creates StringBuffer of size 4
+        StringBuffer newGameCode = new StringBuffer(6);//Creates StringBuffer of size 4
 
         /*
          * for the length of the StringBuffer newGameCode, randomly chooses a character
          * in the String randomString and appends it to newGameCode
          */
-        for(int i = 0; i < newGameCode.length(); i++){
-            for(int j = 0; j < randomString.length(); i++) {
-                newGameCode.append(randomString.charAt(j));
-            }
+        for(int i = 0; i < 6; i++){
+            int index = (int)(Math.random() * randomString.length());
+            newGameCode.append(randomString.charAt(index));
         }
         //Sets the Room gameRoom's code to newGameCode
-        gameRoom.roomCode = newGameCode.toString();
+        newGame.roomCode = newGameCode.toString();
+        newGame.addPlayer(playerName);
+        gameRoom.add(newGame);
+
         return newGameCode.toString();
     }
 
     private void addPlayers(String roomCode, String playerName){
-        Player person = new Player(playerName);
-        if(roomCode.equals(gameRoom.roomCode)) {
-            gameRoom.players.add(person);
-        } else{
-            System.out.println("Fuck you Error");
-        }
 
+        for(Room game: gameRoom){
+            if(game.roomCode.equals(roomCode)){
+                game.addPlayer(playerName);
+                return;
+            }
+        }
+        throw new RuntimeException("Can't find room");
     }
 
     void startGame(String roomCode){
-        gameStarted = true;
-
-        //Get yelp API and store list of restaurant in Room
-        gameRoom.restaurants.add("Yelp APi");
+        for(Room game: gameRoom){
+            if(game.roomCode.equals(roomCode)){
+                // TODO: 11/22/2020 yelp API sets restaurants
+                game.gameStarted = true;
+                return;
+            }
+        }
 
     }
 
-    private boolean confirmRestaurants(String roomCode, String playerName, String restaurant, boolean confirm){
-        String chooseRestaurant = restaurant;
-        if(!gameRoom.roomCode.equals(roomCode)) {
-            return false;
-        }
-        if(!(count > gameRoom.restaurants.size() / 2)) {
-            for (int i = 0; i < gameRoom.restaurants.size(); i++) {
-                if (confirm) {
-                    if (chooseRestaurant.equals(gameRoom.restaurants.indexOf(i))) {
-
-                        count++;
-                    }
+    private void confirmRestaurants(String roomCode, String playerName, String restaurant){
+        for(Room game: gameRoom){
+            if(game.roomCode.equals(roomCode)){
+                Integer temp = game.restaurantMap.get(restaurant);
+                temp++;
+                game.restaurantMap.put(restaurant,temp);
+                if(temp == game.players.size()){
+                    game.winner = true;
                 }
             }
-        } else{
-            return true;
         }
-        return false;
     }
 
 
     Object getGameState(){
         Object hi = new Object();
-        if(gameStarted){
-            return hi;
-        }
+
         return hi;
+    }
+
+    public static void main(String[] args){
+        GameStateManager game = new GameStateManager();
+        String code = game.createNewGame("Tina");
+        System.out.println(code);
+        game.addPlayers(code, "Colin");
     }
 }
--- java/server/src/main/java/food/ws/gamestate/Room.java
+++ java/server/src/main/java/food/ws/gamestate/Room.java
@@ -3,16 +3,24 @@ package food.ws.gamestate;
 import food.ws.gamestate.Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Room{
 
-    static ArrayList<Player> players;
-    ArrayList<String> restaurants;
+    ArrayList<Player> players;
     String roomCode = "";
+    public boolean gameStarted = false;
+    HashMap<String, Integer> restaurantMap;
+    boolean winner = false;
 
-    public Room(){
+    public Room(){ }
 
+    public void addPlayer(String playerName){
+        Player person = new Player(playerName);
+        players.add(person);
     }
+
+
 
 //    public static ArrayList<Player> getPlayers(){
 //        ArrayList<Player> temp = new ArrayList<Player>();
